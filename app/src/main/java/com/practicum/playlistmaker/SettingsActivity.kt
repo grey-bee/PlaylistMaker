dab003ec942da943e1ad3b3df704b22774496da2
Andrey Pchelintsev <doctor.madbee@gmail.com>
2024-11-05 10:24:50 +0700

    added: switch colors for dark theme
    changed:

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -3,6 +3,7 @@ package com.practicum.playlistmaker
 import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
@@ -10,9 +11,9 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val backButton = findViewById<TextView>(R.id.button_back)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
 
-        backButton.setOnClickListener {
+        toolbar.setNavigationOnClickListener{
             val mainScreenIntent = Intent(this, MainActivity::class.java)
             startActivity(mainScreenIntent)
         }
