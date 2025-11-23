48a53da11e436bffd3417a758a4219965af224f1
grey-bee <doctor.madbee@gmail.com>
2025-11-23 23:05:00 +0700

    домашка спринта 9.

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -2,20 +2,34 @@ package com.practicum.playlistmaker
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
-import android.widget.Toolbar
+import  com.google.android.material.switchmaterial.SwitchMaterial
+import com.google.android.material.appbar.MaterialToolbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        val darkThemeSwitch = findViewById<SwitchMaterial>(R.id.themeSwitcher)
+        val currentNightMode = AppCompatDelegate.getDefaultNightMode()
+        darkThemeSwitch.isChecked = (currentNightMode == AppCompatDelegate.MODE_NIGHT_YES)
 
-        toolbar.setNavigationOnClickListener{
+        darkThemeSwitch.setOnCheckedChangeListener { _, checked ->
+            if (checked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+
+        toolbar.setNavigationOnClickListener {
             val mainScreenIntent = Intent(this, MainActivity::class.java)
             startActivity(mainScreenIntent)
         }
+
+
     }
 }
