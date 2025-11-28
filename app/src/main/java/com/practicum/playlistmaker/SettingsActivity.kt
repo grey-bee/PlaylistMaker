7ba8df3a75e94eb65be5f0668d6ea86edf96f7ea
grey-bee <doctor.madbee@gmail.com>
2025-11-28 08:40:48 +0700

    домашка спринта 9.2

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,7 +1,9 @@
 package com.practicum.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.TextView
 import  com.google.android.material.switchmaterial.SwitchMaterial
 import com.google.android.material.appbar.MaterialToolbar
 import androidx.appcompat.app.AppCompatActivity
@@ -11,10 +13,14 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        val currentNightMode = AppCompatDelegate.getDefaultNightMode()
 
         val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
         val darkThemeSwitch = findViewById<SwitchMaterial>(R.id.themeSwitcher)
-        val currentNightMode = AppCompatDelegate.getDefaultNightMode()
+        val shareApp = findViewById<TextView>(R.id.shareApp)
+        val writeToSupport = findViewById<TextView>(R.id.writeToSupport)
+        val userAgreement = findViewById<TextView>(R.id.userAgreement)
+
         darkThemeSwitch.isChecked = (currentNightMode == AppCompatDelegate.MODE_NIGHT_YES)
 
         darkThemeSwitch.setOnCheckedChangeListener { _, checked ->
@@ -30,6 +36,38 @@ class SettingsActivity : AppCompatActivity() {
             startActivity(mainScreenIntent)
         }
 
+        shareApp.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            val chooserTitle = getString(R.string.share_app)
 
+            intent.type = "text/plain"
+            intent.putExtra(
+                Intent.EXTRA_TEXT,
+                "Ссылка на курс: https://practicum.yandex.ru/android-developer/"
+            )
+            startActivity(Intent.createChooser(intent, chooserTitle))
+        }
+
+        writeToSupport.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO)
+            val chooserTitle = getString(R.string.write_to_support)
+
+            intent.data = Uri.parse("mailto:andrey@izoor.com")
+            intent.putExtra(
+                Intent.EXTRA_SUBJECT,
+                "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            )
+            intent.putExtra(
+                Intent.EXTRA_TEXT,
+                "Спасибо разработчикам и разработчицам за крутое приложение!"
+            )
+            startActivity(Intent.createChooser(intent, chooserTitle))
+        }
+
+        userAgreement.setOnClickListener {
+            val url = Uri.parse("https://yandex.ru/legal/practicum_offer/")
+            val intent = Intent(Intent.ACTION_VIEW, url)
+            startActivity(intent)
+        }
     }
 }
