78065dc2a78324822253c12179fcbe38024d7a64
grey-bee <doctor.madbee@gmail.com>
2025-12-03 17:49:04 +0700

    домашка спринта #11-1

--- app/src/main/java/com/practicum/playlistmaker/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/Track.kt
@@ -1,8 +1,14 @@
 package com.practicum.playlistmaker
 
+import com.google.gson.annotations.SerializedName
+
 data class Track(
     val trackName: String, // Название композиции
     val artistName: String, // Имя исполнителя
-    val trackTime: String, // Продолжительность трека
+    @SerializedName("trackTimeMillis") val trackTime: Int, // Продолжительность трека
     val artworkUrl100: String // Ссылка на изображение обложки
+)
+
+data class TrackResponse(
+    val results: List<Track>
 )
