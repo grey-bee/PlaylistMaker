cf14f0c4d8ba417c194fd33139028c35fd88c7dc
grey-bee <doctor.madbee@gmail.com>
2025-12-10 09:28:34 +0700

    домашка спринта #12-1 Добавлен App класс. сохранение информации о темной теме.

---
+++ app/src/main/java/com/practicum/playlistmaker/App.kt
@@ -0,0 +1,32 @@
+package com.practicum.playlistmaker
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.edit
+
+class App : Application() {
+    var darkTheme = false
+    override fun onCreate() {
+        super.onCreate()
+        val sharedPrefs = getSharedPreferences("app_prefs", MODE_PRIVATE)
+        darkTheme = sharedPrefs.getBoolean("dark_theme", false)
+        switchTheme(darkTheme)
+    }
+
+    fun switchTheme(darkThemeEnabled: Boolean) {
+        darkTheme = darkThemeEnabled
+
+        getSharedPreferences("app_prefs", MODE_PRIVATE)
+            .edit {
+                putBoolean("dark_theme", darkThemeEnabled)
+            }
+
+        AppCompatDelegate.setDefaultNightMode(
+            if (darkThemeEnabled) {
+                AppCompatDelegate.MODE_NIGHT_YES
+            } else {
+                AppCompatDelegate.MODE_NIGHT_NO
+            }
+        )
+    }
+}
