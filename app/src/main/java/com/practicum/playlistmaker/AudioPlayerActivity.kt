a714d804bea10ddeb2d3e4ad8912ebf225ed376c
grey-bee <doctor.madbee@gmail.com>
2025-12-17 23:50:21 +0700

    Спринт 14.1

--- app/src/main/java/com/practicum/playlistmaker/AudioPlayerActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/AudioPlayerActivity.kt
@@ -1,6 +1,11 @@
 package com.practicum.playlistmaker
 
+import android.annotation.SuppressLint
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
@@ -18,20 +23,97 @@ import java.util.Locale
 import java.util.TimeZone
 
 class AudioPlayerActivity : AppCompatActivity() {
+
+    companion object {
+        private const val STATE_DEFAULT = 0
+        private const val STATE_PREPARED = 1
+        private const val STATE_PLAYING = 2
+        private const val STATE_PAUSED = 3
+
+        private const val DELAY = 500L
+    }
+
+    private var playerState = STATE_DEFAULT
+    private val mediaPlayer = MediaPlayer()
+    private lateinit var buttonPlay: ImageButton
+    private lateinit var url: String
+    private lateinit var playingTime: TextView
+    private val handler = Handler(Looper.getMainLooper())
+
+    private fun preparePlayer() {
+        mediaPlayer.setOnErrorListener { mp, what, extra ->
+            Log.e("AudioPlayer", "MediaPlayer error: what=$what, extra=$extra")
+            true
+        }
+        Log.d("AudioPlayer", "preparePlayer started, url=$url")
+        mediaPlayer.setDataSource(url)
+        Log.d("AudioPlayer", "setDataSource done")
+        mediaPlayer.prepareAsync()
+        Log.d("AudioPlayer", "prepareAsync called")
+        mediaPlayer.setOnPreparedListener {
+            buttonPlay.isEnabled = true
+            playerState = STATE_PREPARED
+        }
+        mediaPlayer.setOnCompletionListener {
+            buttonPlay.setImageResource(R.drawable.button_play)
+            handler.removeCallbacksAndMessages(null)
+            playingTime.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(0)
+            playerState = STATE_PREPARED
+        }
+    }
+
+    private fun startPlayer() {
+        mediaPlayer.start()
+        startTimer()
+        buttonPlay.setImageResource(R.drawable.button_pause)
+        playerState = STATE_PLAYING
+    }
+
+    private fun pausePlayer() {
+        mediaPlayer.pause()
+        handler.removeCallbacksAndMessages(null)
+        buttonPlay.setImageResource(R.drawable.button_play)
+        playerState = STATE_PAUSED
+    }
+
+    private fun playbackControl() {
+        when (playerState) {
+            STATE_PLAYING -> {
+                pausePlayer()
+            }
+
+            STATE_PREPARED, STATE_PAUSED -> {
+                startPlayer()
+            }
+        }
+    }
+
+    private fun startTimer() {
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                val data = mediaPlayer.getCurrentPosition()
+                val seconds = data / 1000
+                playingTime.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(data)
+                handler.postDelayed(this, DELAY)
+            }
+        }, DELAY)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_audio_player)
-
         val track = intent.getParcelableExtra("track", Track::class.java)
         val buttonBack = findViewById<ImageView>(R.id.backArrow)
         val albumCover = findViewById<ImageView>(R.id.albumCover)
         val trackName = findViewById<TextView>(R.id.trackName)
         val artistName = findViewById<TextView>(R.id.artistName)
         val buttonAddToPlaylist = findViewById<ImageButton>(R.id.buttonAddToPlaylist) //todo
-        val buttonPlay = findViewById<ImageButton>(R.id.buttonPlay) //todo
+        buttonPlay = findViewById(R.id.buttonPlay)
+        buttonPlay.isEnabled = false
         val buttonLike = findViewById<ImageButton>(R.id.buttonLike) //todo
-        val playingTime = findViewById<TextView>(R.id.playingTime)
+        playingTime = findViewById(R.id.playingTime)
         val durationData = findViewById<TextView>(R.id.durationData)
         val albumData = findViewById<TextView>(R.id.albumData)
         val yearData = findViewById<TextView>(R.id.yearData)
@@ -42,11 +124,19 @@ class AudioPlayerActivity : AppCompatActivity() {
 
         trackName.text = track?.trackName
         artistName.text = track?.artistName
-        playingTime.text =  SimpleDateFormat("mm:ss", Locale.getDefault()).apply {
+        playingTime.text = SimpleDateFormat("mm:ss", Locale.getDefault()).apply {
             timeZone = TimeZone.getTimeZone("UTC")
         }.format(0)
         durationData.text = track?.trackTime
-
+        val test = track?.previewUrl.toString()
+        Log.d("debug", test)
+        if (!track?.previewUrl.isNullOrEmpty()) {
+            url = track?.previewUrl.toString()
+            preparePlayer()
+        }
+        buttonPlay.setOnClickListener {
+            playbackControl()
+        }
 
         genreData.text = track?.primaryGenreName
         countryData.text = track?.country
@@ -71,6 +161,8 @@ class AudioPlayerActivity : AppCompatActivity() {
 
 
         buttonBack.setOnClickListener {
+            mediaPlayer.stop()
+            handler.removeCallbacksAndMessages(null)
             finish()
         }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -85,6 +177,16 @@ class AudioPlayerActivity : AppCompatActivity() {
         }
     }
 
+    override fun onPause() {
+        super.onPause()
+        pausePlayer()
+        handler.removeCallbacksAndMessages(null)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
+    }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -2,6 +2,8 @@ package com.practicum.playlistmaker
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.text.TextWatcher
 import android.text.Editable
 import android.view.inputmethod.EditorInfo
@@ -20,13 +22,120 @@ import retrofit2.Response
 import retrofit2.Callback
 import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.ProgressBar
 
 class SearchActivity : AppCompatActivity() {
 
+    companion object {
+        private const val SEARCH_DEBOUNCE_DELAY = 2000L
+        private const val CLICK_DEBOUNCE_DELAY = 1000L
+    }
+
     private var tracks = arrayListOf<Track>()
     private var historyTracks = arrayListOf<Track>()
-    var searchRequest = ""
-    lateinit var searchField: EditText //вынужденная мера так как в задании нужно восстанавливать в onRestoreInstanceState
+    private var searchRequest = ""
+    lateinit var searchField: EditText
+    lateinit var recyclerView: RecyclerView
+    lateinit var nothingFoundLayer: LinearLayout
+    lateinit var noConnectionLayer: LinearLayout
+    lateinit var layoutHistory: LinearLayout
+    lateinit var progress: ProgressBar
+    private var isClickAllowed = true
+    private val handler = Handler(Looper.getMainLooper())
+
+    private fun clickDebounce(): Boolean {
+        val current = isClickAllowed
+        if (isClickAllowed) {
+            isClickAllowed = false
+            handler.postDelayed({ isClickAllowed = true }, CLICK_DEBOUNCE_DELAY)
+        }
+        return current
+    }
+
+
+    private val trackAdapter = TrackAdapter(
+        tracks,
+        onItemClick = { item ->
+            if (clickDebounce()) {
+                SearchHistoryManager.addTrack(item)
+                val audioPlayerDisplayIntent = Intent(this, AudioPlayerActivity::class.java)
+                audioPlayerDisplayIntent.putExtra("track", item)
+                startActivity(audioPlayerDisplayIntent)
+            }
+        }
+    )
+
+    private fun showContent() {
+        recyclerView.isVisible = true
+        nothingFoundLayer.isVisible = false
+        noConnectionLayer.isVisible = false
+    }
+
+    private fun showNothingFound() {
+        recyclerView.isVisible = false
+        nothingFoundLayer.isVisible = true
+        noConnectionLayer.isVisible = false
+    }
+
+    private fun showError() {
+        recyclerView.isVisible = false
+        nothingFoundLayer.isVisible = false
+        noConnectionLayer.isVisible = true
+    }
+
+    private fun closeAll() {
+        recyclerView.isVisible = false
+        nothingFoundLayer.isVisible = false
+        noConnectionLayer.isVisible = false
+        val historySource = SearchHistoryManager.getTrackList()
+        layoutHistory.isVisible = historySource.isNotEmpty()
+    }
+
+    private fun performSearch() {
+        recyclerView.isVisible = false
+        nothingFoundLayer.isVisible = false
+        noConnectionLayer.isVisible = false
+        layoutHistory.isVisible = false
+        progress.isVisible = true
+        RetrofitClient.api.search(searchField.text.toString())
+            .enqueue(object : Callback<TrackResponse> {
+                override fun onResponse(
+                    call: Call<TrackResponse>,
+                    response: Response<TrackResponse>
+                ) {
+                    if (response.isSuccessful) {
+                        val resultList = response.body()?.results
+                        tracks.clear()
+                        if (!resultList.isNullOrEmpty()) {
+                            progress.isVisible = false
+                            showContent()
+                            tracks.addAll(resultList)
+                            trackAdapter.notifyDataSetChanged()
+                        } else {
+                            progress.isVisible = false
+                            showNothingFound()
+                        }
+                    } else {
+                        progress.isVisible = false
+                        showError()
+                    }
+                }
+
+                override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                    progress.isVisible = false
+                    showError()
+                }
+            })
+
+    }
+
+    private val searchRunnable = Runnable { performSearch() }
+
+    private fun searchDebounce() {
+        handler.removeCallbacks(searchRunnable)
+        handler.postDelayed(searchRunnable, SEARCH_DEBOUNCE_DELAY)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,23 +143,17 @@ class SearchActivity : AppCompatActivity() {
         setContentView(R.layout.activity_search)
         val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
         searchField = findViewById(R.id.search_field)
-        val layoutHistory = findViewById<LinearLayout>(R.id.layout_history)
+        recyclerView = findViewById(R.id.recycle_view)
+        nothingFoundLayer = findViewById(R.id.nothing_found)
+        noConnectionLayer = findViewById(R.id.no_connection)
+        progress = findViewById(R.id.progress)
+        layoutHistory = findViewById(R.id.layout_history)
         val historyRecyclerView = findViewById<RecyclerView>(R.id.recycle_view_history)
         val clearButton = findViewById<ImageView>(R.id.button_clear)
         val historyClearButton = findViewById<Button>(R.id.history_clear)
         val refreshButton = findViewById<Button>(R.id.refresh_button)
-        val nothingFoundLayer = findViewById<LinearLayout>(R.id.nothing_found)
-        val noConnectionLayer = findViewById<LinearLayout>(R.id.no_connection)
-        val recyclerView = findViewById<RecyclerView>(R.id.recycle_view)
-        val trackAdapter = TrackAdapter(
-            tracks,
-            onItemClick = { item ->
-                SearchHistoryManager.addTrack(item)
-                val audioPlayerDisplayIntent = Intent(this, AudioPlayerActivity::class.java)
-                audioPlayerDisplayIntent.putExtra("track", item)
-                startActivity(audioPlayerDisplayIntent)
-            }
-        )
+
+
         val historyAdapter = TrackAdapter(
             historyTracks,
             onItemClick = { item ->
@@ -59,37 +162,10 @@ class SearchActivity : AppCompatActivity() {
                 startActivity(audioPlayerDisplayIntent)
             }
         )
-
         recyclerView.isVisible = true
         recyclerView.adapter = trackAdapter
 
         historyRecyclerView.adapter = historyAdapter
-
-        fun showContent() {
-            recyclerView.isVisible = true
-            nothingFoundLayer.isVisible = false
-            noConnectionLayer.isVisible = false
-        }
-
-        fun showNothingFound() {
-            recyclerView.isVisible = false
-            nothingFoundLayer.isVisible = true
-            noConnectionLayer.isVisible = false
-        }
-
-        fun showError() {
-            recyclerView.isVisible = false
-            nothingFoundLayer.isVisible = false
-            noConnectionLayer.isVisible = true
-        }
-
-        fun closeAll() {
-            recyclerView.isVisible = false
-            nothingFoundLayer.isVisible = false
-            noConnectionLayer.isVisible = false
-            val historySource = SearchHistoryManager.getTrackList()
-            layoutHistory.isVisible = historySource.isNotEmpty()
-        }
 
         fun updateHistory() {
             val historySource = SearchHistoryManager.getTrackList()
@@ -119,37 +195,6 @@ class SearchActivity : AppCompatActivity() {
             else layoutHistory.isVisible = false
         }
 
-        fun performSearch() {
-            RetrofitClient.api.search(searchField.text.toString())
-                .enqueue(object : Callback<TrackResponse> {
-                    override fun onResponse(
-                        call: Call<TrackResponse>,
-                        response: Response<TrackResponse>
-                    ) {
-                        if (response.isSuccessful) {
-                            val resultList = response.body()?.results
-                            tracks.clear()
-                            if (!resultList.isNullOrEmpty()) {
-                                showContent()
-                                tracks.addAll(resultList)
-                                trackAdapter.notifyDataSetChanged()
-                            } else {
-                                showNothingFound()
-                            }
-                        } else {
-                            showError()
-                        }
-                    }
-
-                    override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
-                        showError()
-                    }
-                })
-
-        }
-
-
-
         clearButton.setOnClickListener {
             searchField.text.clear()
             val inputMethodManager =
@@ -167,26 +212,14 @@ class SearchActivity : AppCompatActivity() {
         }
 
         val searchWatcher = object : TextWatcher {
-            override fun beforeTextChanged(
-                s: CharSequence?,
-                start: Int,
-                count: Int,
-                after: Int
-            ) {
-                // empty
-            }
-
-            override fun onTextChanged(
-                s: CharSequence?,
-                start: Int,
-                before: Int,
-                count: Int
-            ) {
+            override fun beforeTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 if (s.isNullOrEmpty()) {
                     clearButton.isVisible = false
                     if (historyTracks.isNotEmpty()) layoutHistory.isVisible = true
                 } else {
                     clearButton.isVisible = true
+                    searchDebounce()
                     layoutHistory.isVisible = false
                 }
             }
@@ -194,13 +227,6 @@ class SearchActivity : AppCompatActivity() {
             override fun afterTextChanged(s: Editable?) {
                 searchRequest = s.toString()
             }
-        }
-
-        searchField.setOnEditorActionListener { _, actionId, _ ->
-            if (actionId == EditorInfo.IME_ACTION_DONE) {
-                performSearch()
-            }
-            false
         }
 
         searchField.addTextChangedListener(searchWatcher)
