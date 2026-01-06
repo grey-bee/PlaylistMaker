bfdb603c56d5c258d2603059e72cc9dcea9cc041
Andrey Pchelintsev <doctor.madbee@gmail.com>
2026-01-06 22:03:23 +0700

    Спринт 19.2 MediaLibraryFragment.kt подключен, активити удалена.

--- app/src/main/java/com/practicum/playlistmaker/medialibrary/ui/MediaLibraryPagerAdapter.kt
+++ app/src/main/java/com/practicum/playlistmaker/medialibrary/ui/MediaLibraryPagerAdapter.kt
@@ -1,10 +1,12 @@
 package com.practicum.playlistmaker.medialibrary.ui
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
 
-class MediaLibraryPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+class MediaLibraryPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+    FragmentStateAdapter(fragmentManager, lifecycle) {
     override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> FeaturedTracksFragment()
