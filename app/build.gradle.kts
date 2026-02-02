5ab04a8e5c8d5dee1ef0d326c02b5db3b03a2679
Andrey Pchelintsev <doctor.madbee@gmail.com>
2026-02-02 20:20:47 +0700

    Спринт 27 начало.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.room.ktx)
     implementation(libs.room.runtime)
     implementation(libs.constraintlayout)
+    implementation(libs.androidx.runtime)
     ksp(libs.room.compiler)
     implementation(libs.okhttp)
     implementation(libs.glide)
---
+++ app/src/main/java/com/practicum/playlistmaker/settings/ui/compose/SettingsScreen.kt
@@ -0,0 +1,7 @@
+package com.practicum.playlistmaker.settings.ui.compose
+
+import androidx.compose.runtime.Composable
+
+@Composable
+class SettingsScreen {
+}
